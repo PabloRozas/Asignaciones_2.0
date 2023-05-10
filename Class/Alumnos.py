106008c018d6e2b8e0e15f3ffe78619c611023af
Pablo Macuada <pablo.macuada@usach.cl>
2023-05-10 15:28:42 -0400

    Tutores y Alumnos
    Modificación en los archivo de clases de Tutores y Alumnos. Se define la clase

---
+++ Class/Alumnos.py
@@ -0,0 +1,6 @@
+class Alumnos:
+    def __init__(self):
+        self.alumnos = []
+
+    def add_alumno(self, alumno):
+        self.alumnos.append(alumno)
---
+++ Class/Tutores.py
@@ -0,0 +1,6 @@
+class Tutores:
+    def __init__(self):
+        self.tutores = []
+
+    def add_tutor(self, tutor):
+        self.tutores.append(tutor)
