354606ad6e5f750eefd34817674b2c209f15c2dc
Pablo Macuada <pablo.macuada@usach.cl>
2023-06-01 12:28:51 -0400

    Cambio en Orden de Código y en warning_data()

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-from Resource.change_data import transform_data_class
-from Resource.rw_data import get_data
+from Utils.change_data import transform_data_class
+from Utils.rw_data import get_data
 
 
 
@@ -11,7 +11,10 @@ data = get_data("Resource\data.xlsx")
 # Se transforman los datos a objetos de las clases correspondientes
 alumnos_vae, alumnos_pace, alumnos_solicitudes, tutoresC = transform_data_class(data)
 
-alumnos_vae.print_alumnos()
-alumnos_pace.print_alumnos()
-alumnos_solicitudes.print_alumnos()
-tutoresC.print_tutores()
+# alumnos_vae.get_alumno("26843012").print_alumno()
+
+
+# alumnos_vae.print_alumnos()
+# alumnos_pace.print_alumnos()
+# alumnos_solicitudes.print_alumnos()
+# tutoresC.print_tutores()  
