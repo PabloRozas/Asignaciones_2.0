ecd936c95caca9c7d6505a45d8faf7f67ec070bb
Pablo Macuada <pablo.macuada@usach.cl>
2023-06-16 12:24:10 -0400

    Se agrega traducción de la solicitud de área de tutor

--- Utils/change_data.py
+++ Utils/change_data.py
@@ -41,6 +41,8 @@ def transform_data_class(data):
 
 
     for index, fila in vae.iterrows():
+        aux = especialidades(fila)
+
         alumno = Alumno(fila["RUT"], 
                         fila["NOMBRE COMPLETO"], 
                         fila["CARRERA"], 
@@ -54,13 +56,16 @@ def transform_data_class(data):
                         fila["MATRICULADOS 1s2023"],
                         [fila["ÁREA TUTOR 1"], fila["ÁREA TUTOR 2"], fila["ÁREA TUTOR 3"]],
                         [fila["SUBÁREA TUTOR 1"], fila["SUBÁREA TUTOR 2"], fila["SUBÁREA TUTOR 3"]],
-                        [fila["ESPECIALIDAD 1"], fila["ESPECIALIDAD 2"], fila["ESPECIALIDAD 3"]])
+                        aux)
         alumnos_vae.add_alumno(alumno)
     # alumnos_vae.print_alumnos()
 
     # alumnos_vae.get_alumno("21705466").print_alumno()
 
     for index, fila in pace.iterrows():
+
+        aux = especialidades(fila)
+
         alumno = Alumno(fila["RUT"], 
                         fila["NOMBRE COMPLETO"], 
                         fila["CARRERA"], 
@@ -74,11 +79,15 @@ def transform_data_class(data):
                         fila["MATRICULADOS 1s2023"],
                         [fila["ÁREA TUTOR 1"], fila["ÁREA TUTOR 2"], fila["ÁREA TUTOR 3"]],
                         [fila["SUBÁREA TUTOR 1"], fila["SUBÁREA TUTOR 2"], fila["SUBÁREA TUTOR 3"]],
-                        [fila["ESPECIALIDAD 1"], fila["ESPECIALIDAD 2"], fila["ESPECIALIDAD 3"]])
+                        aux)
         alumnos_pace.add_alumno(alumno)
     # alumnos_pace.print_alumnos()
 
     for index, fila in solicitudes.iterrows():
+
+        aux = especialidades(fila)
+
+
         alumno = Alumno(fila["RUT"], 
                         fila["NOMBRE COMPLETO"], 
                         fila["CARRERA"], 
@@ -92,7 +101,7 @@ def transform_data_class(data):
                         fila["MATRICULADOS 1s2023"],
                         [fila["ÁREA TUTOR 1"], fila["ÁREA TUTOR 2"], fila["ÁREA TUTOR 3"]],
                         [fila["SUBÁREA TUTOR 1"], fila["SUBÁREA TUTOR 2"], fila["SUBÁREA TUTOR 3"]],
-                        [fila["ESPECIALIDAD 1"], fila["ESPECIALIDAD 2"], fila["ESPECIALIDAD 3"]])
+                        aux)
         alumnos_solicitudes.add_alumno(alumno)
     # alumnos_solicitudes.print_alumnos()
 
@@ -113,6 +122,97 @@ def transform_data_class(data):
     # tutoresC.get_tutor("20780420").print_tutor()
 
     return [alumnos_vae, alumnos_pace, alumnos_solicitudes, tutoresC]
+
+
+
+def especialidades(fila):
+    aux = []
+    aux2 = []
+    for i in range(1,4):
+        # ! Aqui se pude editar para agregar mas especialidades
+        # ? ------------------ ALGEBRA I Y CALCULO I ------------------
+        if (fila["ESPECIALIDAD " + str(i)] == "ALGEBRA" 
+                or fila["ESPECIALIDAD " + str(i)] == "ALGEBRA I" 
+                or fila["ESPECIALIDAD " + str(i)] == "ALGEBRA I PARA INGENIERIA"
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO I, ALGEBRA I"):
+            aux.append("ALGEBRA I Y CALCULO I")
+        
+        # ? ----------------------- CALCULO I -------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "CALCULO" 
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO I" 
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO I PARA INGENIERIA"
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO TECNO"):
+            aux.append("CALCULO I") 
+
+        # ? ----------------- ALGEBRA II Y CALCULO II -----------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "ALGEBRA II"
+                or fila["ESPECIALIDAD " + str(i)] == "ALGEBRA II PARA INGENIERIA"
+                or fila["ESPECIALIDAD " + str(i)] == "ALGEBRA LINEAL"):
+            aux.append("ALGEBRA II Y CALCULO II")
+
+        # ? ----------------------- CALCULO II ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "CALCULO II"
+                or (fila["ESPECIALIDAD " + str(i)] == "CALCULO AVANZADO" and fila["FACULTAD"] == "FACULTAD TECNOLOGICA")
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO II PARA INGENIERIA"):
+            aux.append("CALCULO II")
+
+        # ? ----------------------- CALCULO III -----------------------
+        elif ((fila["ESPECIALIDAD " + str(i)] == "CALCULO AVANZADO" and fila["FACULTAD"] != "FACULTAD TECNOLOGICA")
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO III"
+                or fila["ESPECIALIDAD " + str(i)] == "CALCULO III PARA INGENIERIA"):
+            aux.append("CALCULO III")
+
+        # ? ----------------------- MATEMATICAS I ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS I"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADM. Y ECONOMIA I"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADMINISTRACION Y ECONOMIA I"):
+            aux.append("MATEMATICA I")
+
+        # ? ----------------------- MATEMATICAS II ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "MATEMATICA PARA LA ADM. Y ECONOMIA II"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS II"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADM. Y ECONOMIA II"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADMINISTRACION II"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADMINISTRACION Y ECONOMIA II"):
+            aux.append("MATEMATICA II")
+
+        # ? ----------------------- MATEMATICAS III ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "MATEMATICA III"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS III"
+                or fila["ESPECIALIDAD " + str(i)] == "MATEMATICAS PARA LA ADM. Y ECONOMIA III"):
+            aux.append("MATEMATICA III")
+
+        # ? ----------------------- ECUACIONES DIFERENCIALES ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "ECUACIONES DIFERENCIALES"
+                or fila["ESPECIALIDAD " + str(i)] == "ECUACIONES DIFERENCIALES PARA INGENIERIA"
+                or fila["ESPECIALIDAD " + str(i)] == "ECUACIONES DIFERENCIALES Y METODOS NUMERICOSA"):
+            aux.append("ECUACIONES DIFERENCIALES")
+
+        # ? ----------------------- ESTADISTICA Y PROBABILIDAD ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "ANALISIS ESTADISTICO"
+                or fila["ESPECIALIDAD " + str(i)] == "ANALISIS ESTADISTICO PARA INGENIERIA"
+                or fila["ESPECIALIDAD " + str(i)] == "ESTADISTICA DESCRIPTIVA"
+                or fila["ESPECIALIDAD " + str(i)] == "ESTADISTICA II"
+                or fila["ESPECIALIDAD " + str(i)] == "ESTATICA APLICADA"):
+            aux.append("ESTADISTICA Y PROBABILIDAD")
+
+        # ? ----------------------- PENSAMIENTO MATEMATICO ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "PENSAMIENTO MATEMATICO"):
+            aux.append("PENSAMIENTO MATEMATICO")
+
+        # ? ----------------------- ASIGNACIONES MANUALES ------------------------
+        elif (fila["ESPECIALIDAD " + str(i)] == "PENSAMIENTO LOGICO MATEMATICO"
+                or fila["ESPECIALIDAD " + str(i)] == "RAZONAMIENTO LOGICO-MATEMATICO"):
+            aux.append("ASIGNAR MANUALMENTE")
+        else:
+            aux.append("NO ASIGNADO")
+        
+    # Eliminar repetidos en la lista auxiliar
+    for i in aux:
+        if (i not in aux2 and not(len(aux2) > 0 and i == "NO ASIGNADO")):
+            aux2.append(i)
+
+    return aux2
 
 
 
