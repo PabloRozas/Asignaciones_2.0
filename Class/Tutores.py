6276f6613eea63793a8929e98ceedf8c701be826
Pablo Macuada <pablo.macuada@usach.cl>
2023-06-22 11:53:03 -0400

    Se agrega método en Tutores para transformar a csv

--- Class/Tutores.py
+++ Class/Tutores.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class Tutores:
     def __init__(self):
         self.tutores = []
@@ -13,6 +15,9 @@ class Tutores:
             if tutor.rut == rut:
                 return tutor
         return None
+    
+    def get_total(self):
+        return len(self.tutores)
     
     def print_tutores(self):
 
@@ -46,4 +51,14 @@ class Tutores:
             if tutor.area == "IDIOMAS":
                 tutores.append(tutor)
         return tutores
+    
+    def to_csv(self, path):
+        # Se transforma la lista de tutores a un dataframe
+        df = pd.DataFrame(columns=['rut', 'nombre_completo', 'carrera', 'facultad', 'correo_usach', 'telefono_1', 'correo_personal', 'area', 'subarea', 'horas'])
+        for tutor in self.tutores:
+            new = pd.DataFrame({'rut': [tutor.rut], 'nombre_completo': [tutor.nombre_completo], 'carrera': [tutor.carrera], 'facultad': [tutor.facultad], 'correo_usach': [tutor.correo_usach], 'telefono_1': [tutor.telefono_1], 'correo_personal': [tutor.correo_personal], 'area': [tutor.area], 'subarea': [tutor.subarea], 'horas': [tutor.horas]})
+            df = pd.concat([df, new], ignore_index=True)
+
+        # Se guarda el dataframe en un archivo csv
+        df.to_csv(path, index=False, header=True)
     
